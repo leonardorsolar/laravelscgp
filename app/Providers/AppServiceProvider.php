9212caf8b0fe158291cb4c960a8a5a99fbd02019
Solar <leonardosolar@yahoo.com.brr>
2018-12-08 17:37:30 -0200

    Login instalado

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,9 +1,8 @@
 <?php
-
 namespace App\Providers;
-
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
+ 
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -13,9 +12,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Schema::defaultStringLength(191);
     }
-
+ 
     /**
      * Register any application services.
      *
@@ -25,4 +24,5 @@ class AppServiceProvider extends ServiceProvider
     {
         //
     }
+ 
 }
